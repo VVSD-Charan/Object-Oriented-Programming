31d3a45e7ec306249d42fe71a60ee86dadfa9fce
VVSD-Charan <20bcs244@iiitdmj.ac.in>
2024-01-02 19:14:09 +0530

    modified:   constVariables.cpp

--- constVariables.cpp
+++ constVariables.cpp
@@ -10,10 +10,11 @@ class Person
     // b is declared next 
     const int b;
 
-    Person(int a):a(a),b(2*a){}
+    Person(int x):a(x),b(2*a){}
 
-    // Person(int a):b(a),a(b){}
-    // This is wrong  because a is declared first , so a must be assigned value first and then b
+    // Person(int x):b(x),a(2*b){}
+    // This is wrong  because a is declared first , so value of a will be assigned first. But as b is not yet assigned , a value will be garbage
+    // This concept is called initialization list where const variables declared first will be assigned values first 
 };
 
 int main()
